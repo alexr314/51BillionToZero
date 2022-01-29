42f792b9be069a1493e858ee595e87716b11e1d8
Alex Roman <alexroman@Alexs-MacBook-Pro-4.local>
2022-01-29 13:20:44 -0500

    message2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const myNum = document.getElementById("my-num");
 
 document.getElementById("my-button").onclick = () => {
-    myNum.innerHTML = (parseInt(myNum.innerHTML) + 1).toString();
+    myNum.innerHTML = (parseInt(myNum.innerHTML) + 2).toString();
 }
